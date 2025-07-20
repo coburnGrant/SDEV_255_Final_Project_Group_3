e0f97bd934604c9c690d6b011e7d6886c743fd03
jessi <jcoplen2@ivytech.edu>
2025-07-20 09:29:45 -0400

    add backend folder for project

---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://jcoplen2:hJmPOpv3bmPR9ydr@courses.dea4w3h.mongodb.net/?retryWrites=true&w=majority&appName=Courses")
+
+module.exports = mongoose

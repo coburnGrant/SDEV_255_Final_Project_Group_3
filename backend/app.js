9700f3dd1f593d4f6f2a46db9404452cb6f3b59b
jessi <jcoplen2@ivytech.edu>
2025-07-25 12:23:09 -0400

    user/auth/status routes added to backend

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,83 @@ const express = require("express")
 var cors = require('cors')
 
 const bodyParser = require('body-parser')
+const jwt = require('jwt-simple')
 const Course = require("./models/courses")
+const Teacher = require("./models/teachers")
+const User = require("./models/user")
 const app = express()
 app.use(cors())
 
 app.use(express.json())
 
 const router = express.Router()
+const secret = "supersecret"
+
+//create a new user
+router.post("./user", async function(req, res){
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+    }
+    
+    const newUser = await new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    })
+
+    try{
+        await newUser.save()
+        res.sendStatus(201)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
+router.post("/auth", async(req, res)=>{
+if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username or password"})
+        return
+    }
+let user = await User.findOne({username: req.body.username})
+    if(!user){
+        res.status(401).json({error: "Bad username"})
+    }
+    else{
+        if(user.password != req.body.password){
+            res.status(401).json({error: "Bad password"})
+        }
+        else{
+            username2 = user.username
+            const token = jwt.encode({username: user.username}, secret)
+            const auth = 1
+
+            res.json({
+                username2,
+                token:token,
+                auth:auth
+            })
+        }
+    }
+
+})
+
+router.get("/status", async(req,res)=>{
+    if(!req.headers["x-auth"]){
+        return res.status(401).json({error: "Missing X-Auth"})
+    }
+    const token = req.headers["x-auth"]
+    try{
+    const decoded = jwt.decode(token,secret)
+
+    let users = User.find({}, "username status")
+    res.json(users)
+}
+catch(ex){
+    res.status(401).json({error: "Invalid JWT"})
+}
+
+})
 
 router.get("/courses", async(req,res) =>{
     try{
